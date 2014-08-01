acfacf636766cd6a1753e64e35521f983d1cce2f
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-08-01 11:28:10 -0300

    Trying to implement WSSE

--- src/PROCERGS/NotificationServiceBundle/PROCERGSNotificationServiceBundle.php
+++ src/PROCERGS/NotificationServiceBundle/PROCERGSNotificationServiceBundle.php
@@ -3,7 +3,17 @@
 namespace PROCERGS\NotificationServiceBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use PROCERGS\NotificationServiceBundle\DependencyInjection\Security\Factory\WsseFactory;
 
 class PROCERGSNotificationServiceBundle extends Bundle
 {
+
+    public function build(\Symfony\Component\DependencyInjection\ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new WsseFactory());
+    }
+
 }
